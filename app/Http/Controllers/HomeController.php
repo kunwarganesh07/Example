181a39680aebec810c45b8e53b1c37dead9b36c7
ganesh kunwar <kunwarganesh07>
2025-11-13 15:33:37 +0545

    Secont LCTR

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function student()
+    {
+        return view('student.index');
+    }
+
+    public function create()
+    {
+        return view('student.create');
+    }
+}
